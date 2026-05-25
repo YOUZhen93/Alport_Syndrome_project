da550c29dc06c521b8b5e37ea284df6d76d86435
Zhen You <youzhen93@gmail.com>
2026-05-25 23:27:13 +0800

    update sessionInfo of the env

--- pheno-geno/Bayesian_regression_hearing.R
+++ pheno-geno/Bayesian_regression_hearing.R
@@ -9,6 +9,7 @@ library(ggsci)
 library(cowplot)
 library(ggdist)
 library(tidybayes)
+library(tidyverse)
 # hearing loss
 
 # generate prior
@@ -38,7 +39,7 @@ hear_df = read.table("hearing_loss_data.xls", header = T, sep = "\t")
 hear_df$hearing = as.integer(factor(hear_df$hearing, levels = c("Normal", "Hearing loss")))
 
 # remove genotypes with too few cases
-hear_df %>% group_by(GT) %>% mutate(freq = n()) %>% filter(freq > 10) %>% as.data.frame -> hear_df
+hear_df %>% group_by(genotype) %>% mutate(freq = n()) %>% filter(freq > 10) %>% as.data.frame -> hear_df
 
 
 # setting prior pars; N for total cases number and m for hearing loss number 
@@ -58,7 +59,7 @@ model <- brm(
     family = binomial(link = "logit"),
     data = hear_df,
     prior = prior(beta(1, 1), class = b, lb = 0, ub = 1),
-    chains = 4, warmup = 1000, iter = 2000, seed = 123,
+    chains = 4, warmup = 1000, iter = 2000, seed = 123, # user can increase the iter and chains for convergence
   refresh = 0
 )
 
@@ -84,3 +85,26 @@ axis(side = 1, at = 1:8, labels = rownames(posterior_summary(model))[5:12], las=
 
 
 # eyesight is applied as the same method
+# sessionInfo:
+'''
+R version 4.3.1 (2023-06-16 ucrt)
+Platform: x86_64-w64-mingw32/x64 (64-bit)
+Running under: Windows 11 x64 (build 26200)
+
+Matrix products: default
+
+
+locale:
+[1] LC_COLLATE=English_Canada.utf8  LC_CTYPE=English_Canada.utf8    LC_MONETARY=English_Canada.utf8
+[4] LC_NUMERIC=C                    LC_TIME=English_Canada.utf8    
+
+
+attached base packages:
+[1] stats     graphics  grDevices utils     datasets  methods   base     
+
+other attached packages:
+ [1] lubridate_1.9.3    forcats_1.0.0      stringr_1.5.1      dplyr_1.1.4        purrr_1.0.2       
+ [6] readr_2.1.5        tidyr_1.3.0        tibble_3.2.1       ggplot2_3.5.1      tidyverse_2.0.0   
+[11] tidybayes_3.0.6    ggdist_3.3.2       cowplot_1.1.3      ggsci_3.2.0        bayesrules_0.0.2  
+[16] brms_2.21.0        Rcpp_1.0.11        rstan_2.32.6       StanHeaders_2.32.6
+'''
